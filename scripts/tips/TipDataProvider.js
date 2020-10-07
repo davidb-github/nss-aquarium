2ac298b9cafc744f28056cab74ab915fd9aea316
David Bing <davidb.devops@gmail.com>
2020-10-07 16:34:02 -0500

    implement new script dirs

---
+++ scripts/tips/TipDataProvider.js
@@ -0,0 +1,17 @@
+// array to tip objects
+const tipCollection = [
+    {
+      topic: "Diet",
+      text : "Never feed after midnight"
+    },
+    {
+        topic: "Sleep Schedule",
+        text : "Never wake the fish"
+    },
+    {
+        topic: "Fitness",
+        text : "Swim laps"
+
+    }
+     
+]
