b38fb6cfc42c5fab07602199b3ca1847b21d6351
David Bing <davidb.devops@gmail.com>
2020-10-09 11:13:48 -0500

    update fish object name value for quick filter identification

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -7,7 +7,7 @@
 const fishCollection = [
     {
         img: "https://images.unsplash.com/photo-1522720833375-9c27ffb02a5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
-        name: "Bubbles",
+        name: "holy-fish-1",
         species: "Blue Fin",
         length: 3,
         location: "Costa Rica",
@@ -15,15 +15,15 @@ const fishCollection = [
     },
     {
         img: "https://images.unsplash.com/photo-1576806021995-9f68eb39f10b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80",
-        name: "Bubba",
+        name: "holy-fish-2",
         species: "Green Fin",
-        length: 15,
+        length: 6,
         location: "Florida",
         food: "Carnivore"
     },
     {
         img: "https://images.unsplash.com/photo-1600067156933-bff4d0f0c67f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1646&q=80",
-        name: "Craig",
+        name: "holy-fish-3",
         species: "Yellow Fin",
         length: 9,
         location: "Mexico",
@@ -31,7 +31,7 @@ const fishCollection = [
     },
     {
         img: "https://images.unsplash.com/photo-1600067156933-bff4d0f0c67f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1646&q=80",
-        name: "Alicia",
+        name: "soldier-fish-1",
         species: "Yellow Fin",
         length: 90,
         location: "Mexico",
@@ -39,7 +39,7 @@ const fishCollection = [
     },
     {
         img: "https://images.unsplash.com/photo-1600067156933-bff4d0f0c67f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1646&q=80",
-        name: "Kelly",
+        name: "soldier-fish-3",
         species: "Yellow Fin",
         length: 155,
         location: "Mexico",
@@ -47,7 +47,7 @@ const fishCollection = [
     },
     {
         img: "https://images.unsplash.com/photo-1600067156933-bff4d0f0c67f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1646&q=80",
-        name: "Kelly",
+        name: "soldier-fish-4",
         species: "Yellow Fin",
         length: 155,
         location: "Mexico",
@@ -55,7 +55,7 @@ const fishCollection = [
     },
     {
         img: "https://images.unsplash.com/photo-1600067156933-bff4d0f0c67f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1646&q=80",
-        name: "Paul",
+        name: "common-fish-1",
         species: "Yellow Fin",
         length: 2,
         location: "Mexico",
